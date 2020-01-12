d723c6cdff5e6e0febec34e42c446596490dd920
Brad <github@bnjf.id.au>
2020-01-12 03:17:44 +0000

    allow step as a parameter

--- candidates-prob.pl
+++ candidates-prob.pl
@@ -9,8 +9,9 @@ use Math::GMP ":constant";
 my ( $b, $r, $a );
 
 $b = shift // 256;
+my $step = shift;
 
-for ($r = 1; $r <= 256; $r += $r) {
+for ($r = 1; $r <= 256; $r += $step ? 1 : $r) {
   for ($a = ($b - 2); $a > 0; $a--) {
 
     say "$a*$b**$r-1 ", $a * $b**$r - 1
