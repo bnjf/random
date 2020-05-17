39659f09360ebceab945a3a4d77e9a69ceba516b
Brad <github@bnjf.id.au>
2020-05-17 09:14:08 +0000

    draw 256 chunks instead of 320
    zero page indirect indexing with y does not wrap, the carry is taken
    into the high byte of the word.  so instead, increment the s and r lag
    indexes, which frees up y to be used as an offset into the framebuffer
    to give us a 256x160 view of 20 separate states (there is one bit on the
    right hand side used to persist the carry state)

--- 8/c64/awc.asm
+++ 8/c64/awc.asm
@@ -2,11 +2,7 @@
 	processor 6502
 	org $1000
 
-vid	equ $0400
-
 start:	
-	cli
-
 	; set hires bitmap mode, and video to page 1
 	lda #$3b
 	sta $d011
@@ -17,13 +13,12 @@ start:
 	sta $d018
 
 	lda #$0		; black
-	;sta $d021	; set background colour
 	sta $d020	; set border colour
 
 	; clear colours
-	lda #$04 ; $2000 bit map, $0400 screen
+	lda #$04 	; $0400 palette
 	sta $fc
-	lda #$0
+	lda #$00
 	sta $fb
 
 	; colour data.  screen bitmap is 1-bit, and picks the
@@ -42,40 +37,94 @@ clear:	lda #$20 	; $2000 bit map, $0400 screen
 	sta $fc
 	ldx #$20 	; $2000 - $3fff	
 	ldy #0
-	;lda #0
-loopb:	txa
-	adc #1
-	and #~3
+	lda #0
+clear_loop:
 	sta ($fb),y
 	iny
-	bne loopb
+	bne clear_loop
 	inc $fc
 	dex
-	bne loopb
+	bne clear_loop
 
-;;
-	; lags, x_{i-256} + x_{i-3} + c
-	lda #0		; 256 % 256
-	sta $fb
-	lda #3		; 3   % 256
-	sta $fd
-forever:
+;; seed
+
+	ldy #0
 	lda #$20 	; $2000 bit map, $0400 screen
+	sta $fc
+	ldx #20
+	cli
+	txa
+seed_loop:
+	adc $a2
+	adc #7
+	sta ($fb),y
+	inc $fb
+	bne seed_loop
+
+; spin for screen
+w1: bit $d011
+    bpl w1
+w2: bit $d011
+    bmi w2
+
+	pha
+	tya
+	clc
+	adc #64
+	tay
+	pla
+	bcc seed_next
+	inc $fc
+seed_next:
+	inc $fc		; hi byte
+	dex
+	txa
+	bne seed_loop
+	sei
+
+;; main
+
+	; lags, x_{i-256} + x_{i-3} + c
+	lda #0		; 256 - 256 % 256
+	sta $fb
+	lda #-3		; 256 - 3   % 256
+	sta $fd
+
+	ldy #0
+forever:lda #$20 	; $2000 bit map, $0400 screen
 	sta $fc
 	sta $fe	
 
-	ldx #$1e	; count of blocks.  lcm(320,256)=0x500
-	ldy #$0
-	sec
-loop:	lda ($fb),y
+	ldx #20		; how many blocks to show
+loop:	; grab the saved carry
+	inc $fc
+	lda ($fb),y
+	lsr		; bit 0 -> carry
+	dec $fc
+l:	lda ($fb),y
 	adc ($fd),y
 	sta ($fb),y
-	iny
-	bne loop
-	; next block.  XXX we're discarding carry block to block
+	inc $fd		; s++
+	inc $fb		; r++
+	bne l
+
+	; save carry for this block
 	inc $fc
 	inc $fe
-	dex
+	lda #0
+	rol		; carry -> bit 0
+	sta ($fb),y
+
+	tya
+	clc
+	adc #64
+	tay
+	bcc next
+
+	; catch up the y delta
+	inc $fc
+	inc $fe
+next:	dex
 	bne loop
 	beq forever
 
